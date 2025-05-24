0f813e1a141a9939c82ee44aca1f38a6d2f3295c
HimanshuKumar2971 <144801042+HimanshuKumar2444@users.noreply.github.com>
2025-05-25 02:20:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	str = str.trim();
+
+
+    const spaceIndex = str.indexOf(' ');
+
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Return substring from start to first space
+    return str.slice(0, spaceIndex);
 }
 
 // Do not change the code below
